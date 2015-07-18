848d106861cf96a9330c1600c3441bb571c8b03c
dullone <rjippel@gmail.com>
2015-07-18 09:04:34 -0500

    Add bootstrap styles

--- spec/models/toot_spec.rb
+++ spec/models/toot_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Toot, type: :model do
   let(:toot) { FactoryGirl.create(:toot) }
+  
   describe "message" do
     it "has a max length of 140" do 
       toot.message = "l" * 141
+      expect(toot.save).to eql false
+    end
+    it "has a minimum legnth of 2" do 
+      toot.message = "l" 
       expect(toot.save).to eql false
     end
   end
