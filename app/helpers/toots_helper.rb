5ae54d61173fa7f4d6b3dce0fbd52818a866e6a1
dullone <rjippel@gmail.com>
2016-05-19 10:19:43 -0500

    refactor creating of toot @username links

--- app/helpers/toots_helper.rb
+++ app/helpers/toots_helper.rb
@@ -15,29 +15,49 @@ module TootsHelper
     newToots
   end
 
-  def create_mention_links(message)
-    parsedToot = Toot.parse(message)
-    messageWithLinks = []
+  #def create_mention_links(message)
+  #  parsedToot = Toot.parse(message)
+  #  messageWithLinks = []
+#
+  #  startIndex = 0
+  #  parsedToot[:mentions].each do |u|
+  #    user = User.ci_find_by_username u 
+  #    if user 
+  #      endIndex = message.downcase.index("@" + u) - 1
+  #      if endIndex > 0 #make sure we aren't at toot begining
+  #        messageWithLinks << { message: message[startIndex .. endIndex], type: :text }
+  #      end
+  #      messageWithLinks << { message: message[endIndex + 1 .. endIndex + u.length + 1], 
+  #                            type: :link, link: user_toots_path(user) }
+  #      startIndex = endIndex + u.length + 2 #includes @ symbol
+  #    end
+  #  end
+#
+  #  if startIndex < message.length - 1
+  #    messageWithLinks << {message: message[startIndex .. -1], type: :text}
+  #  end
+  #  
+  #  messageWithLinks
+  #end
 
-    startIndex = 0
-    parsedToot[:mentions].each do |u|
-      user = User.ci_find_by_username u 
-      if user 
-        endIndex = message.downcase.index("@" + u) - 1
-        if endIndex > 0 #make sure we aren't at toot begining
-          messageWithLinks << { message: message[startIndex .. endIndex], type: :text }
-        end
-        messageWithLinks << { message: message[endIndex + 1 .. endIndex + u.length + 1], 
-                              type: :link, user_link: user_toots_path(user) }
-        startIndex = endIndex + u.length + 2 #includes @ symbol
-      end
+  def add_username_links(text)
+    minLength = User::MIN_USERNAME_LENGTH
+    maxLength = User::MAX_USERNAME_LENGTH + 1 # +1 for @ symbol
+    usernames = text.downcase.scan(/@[a-z_]{#{minLength},#{maxLength}}/)
+    usernames.uniq!
+    usernames.each do |username|
+      text.gsub!(username, username_link(username))
     end
+    text
+  end
 
-    if startIndex < message.length - 1
-      messageWithLinks << {message: message[startIndex .. -1], type: :text}
+  def username_link(at_username)
+    username = at_username[1..-1]
+    user = User.ci_find_by_username(username)
+    if user 
+      return link_to at_username, user_path(username)
     end
-    
-    messageWithLinks
+    at_username
   end
 
   def create_mentions(toot)
