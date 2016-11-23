d3a46c6271032c928fb01b157b3d822ea99d1f23
dullone <rjippel@gmail.com>
2016-11-22 19:09:55 -0600

    add paginate to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ autocomplete :user, :username, extra_data: [:name]
   end
 
   def index
-    @users = User.all
+    @users = User.all.paginate(page: params[:page], per_page: 20)
   end
 
   def show
